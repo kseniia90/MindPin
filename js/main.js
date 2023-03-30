0ccca8575a3508a059d45863adf13b158fe7b02f
Kseniia Pukhova <xenio310390@gmail.com>
2023-03-30 18:28:18 +0300

    add styles

--- js/main.js
+++ js/main.js
@@ -1,46 +1,38 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        items:1,
-        nav: true,
-        dots: false,
-        // onDragged: updateCount,
-        next: updateCount,
-        prev: updateCount,
-    });
-    function updateCount(event) {
-        console.log(event.item.count)
-        console.log(event.item.index)
-    }
+$(function () {
+    // slider
+    var owl = $(".owl-carousel").owlCarousel({
+    items: 1,
+    dots: false,
+    onInitialized: updateCount,
   });
+  owl.on('changed.owl.carousel', function(e) { 
+    updateCount(e)
+  })
+  function updateCount(event) {
+    console.log(event.item.count);
+    console.log(event.item.index);
+    $(".active-page").text(event.item.index + 1);
+    $(".pages").text(event.item.count);
+  }
+  $(".next-slide").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
+  $(".prev-slide").click(function () {
+    owl.trigger("prev.owl.carousel");
+  });
+ 
 
-  $(function() {
-	
-	//BEGIN
-	$(".accordion__title").on("click", function(e) {
-
-		e.preventDefault();
-		var $this = $(this);
-
-		if (!$this.hasClass("accordion-active")) {
-			$(".accordion__content").slideUp(400);
-			$(".accordion__title").removeClass("accordion-active");
-			$('.accordion__arrow').removeClass('accordion__rotate');
-		}
-
-		$this.toggleClass("accordion-active");
-		$this.next().slideToggle();
-		$('.accordion__arrow',this).toggleClass('accordion__rotate');
-	});
-	//END
-	
+  // accordion
+  $(".accordion__title").on("click", function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    if (!$this.hasClass("accordion-active")) {
+      $(".accordion__content").slideUp(400);
+      $(".accordion__title").removeClass("accordion-active");
+      $(".accordion__arrow").removeClass("accordion__rotate");
+    }
+    $this.toggleClass("accordion-active");
+    $this.next().slideToggle();
+    $(".accordion__arrow", this).toggleClass("accordion__rotate");
+  });
 });
-// показать меню при клике на бургер, заборонили scroll body при відкритому меню-бургер
-
-const menuIcon = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-if(menuIcon) {menuIcon.addEventListener("click", function menu(e) {
-        document.body.classList.toggle('lock');
-        menuBody.classList.toggle('_active');
-        menuIcon.classList.toggle('_active');
-    })
-}
